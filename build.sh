f29781ab7d80e025ab4d0ef5726109bdd0e8bee1
emily <git@emily.ly>
2023-03-08 16:29:03 +0000

    check if azusa broke

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Restarting..."
+docker compose down
+docker compose up --build -d --remove-orphans
+echo "Waiting to verify successful start..."
+sleep 5
+
+if [[ `docker compose ls | grep -q ^azusa` ]]; then
+    echo "azusa isn't running anymore :("
+    docker compose logs 
+    exit 1
+else
+    echo "azusa still running :)"
+    exit 0
