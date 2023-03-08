9573f4849b18b2718d8845083d5c969ba1101e91
emily <git@emily.ly>
2023-03-08 16:35:32 +0000

    fix conditional
    brain broken

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ docker compose up --build -d --remove-orphans
 echo "Waiting to verify successful start..."
 sleep 5
 
-if [[ `docker compose ls | grep -q ^azusa` ]]; then
+if docker compose ls | grep -q ^azusa; then
+    echo "azusa still running :)"
+    exit 0
+else
     echo "azusa isn't running anymore :("
     docker compose logs 
     exit 1
-else
-    echo "azusa still running :)"
-    exit 0
 fi
