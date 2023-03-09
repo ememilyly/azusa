d0382b676cef6ef171f88e3f2b6a23055d4ecd5f
emily <git@emily.ly>
2023-03-09 17:22:52 +0000

    change to dezgo

--- cogs/ai.py
+++ cogs/ai.py
@@ -5,6 +5,7 @@ import logging
 
 import re
 import requests
+import io
 # import openai
 
 _log = logging.getLogger(__name__)
@@ -28,24 +29,47 @@ class ai(commands.Cog):
             if re.match(mention_regex, word):
                 obj = helpers.get_object_from_mention(ctx, word)
                 if isinstance(obj, discord.Member):
-                    word = obj.nick
+                    if obj.nick:
+                        word = obj.nick
+                    else:
+                        word = obj.name
                 else:
                     word = obj.name
             p.append(word)
 
         prompt = ' '.join(p)
         self.log.info(f"generating image prompt: {prompt}")
+
+        # dezgo
+        url = "https://dezgo.p.rapidapi.com/text2image"
+        payload = {
+            "prompt": prompt,
+            "guidance": 7,
+            "steps": 30,
+            "sampler": "euler_a",
+            "upscale": 1,
+            "negative_prompt": "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft",
+            "model": "epic_diffusion_1_1"
+        }
+        headers = {
+            "content-type": "application/x-www-form-urlencoded",
+            "X-RapidAPI-Key": self.bot.config["ai"]["dezgo_api_key"],
+            "X-RapidAPI-Host": "dezgo.p.rapidapi.com"
+        }
+
         async with ctx.typing():
             r = requests.post(
-                "https://api.deepai.org/api/text2img",
-                data={"text": prompt, "grid_size": "1"},
-                headers={'api-key': self.bot.config["ai"]["deepai_api_key"]}
+                url,
+                data=payload,
+                headers=headers
             )
-            self.log.info(r.json())
-            if "err" in r.json():
-                await ctx.send(r.json()["err"])
-            elif "output_url" in r.json():
-                await ctx.send(r.json()["output_url"])
+
+            await ctx.send(
+                file=discord.File(
+                    io.BytesIO(r.content),
+                    filename="image.png"
+                )
+            )
 
 
 async def setup(bot):
