5258cddab2abf6f7ff012d3426779b78e9008e59
emily <git@emily.ly>
2023-03-09 20:41:01 +0000

    add openai chat stuff

--- cogs/ai.py
+++ cogs/ai.py
@@ -3,11 +3,8 @@ import discord
 from lib import helpers
 import logging
 
-import re
 import requests
 import io
-
-# import openai
 
 _log = logging.getLogger(__name__)
 
@@ -17,36 +14,43 @@ class ai(commands.Cog):
         self.bot = bot
         self.log = _log
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if f"<@{self.bot.user.id}>" in message.content:
+            # prompt = helpers.replace_mentions_with_names(message.context, message.content)
+            self.log.info(message.clean_content)
+            await message.reply(helpers.generate_openai_chat(
+                message.clean_content,
+                self.bot.config["ai"]["openai_api_key"]
+            ))
+
     @commands.command(
         aliases=("t2i",),
         help="Generate images through dezgo text2img",
     )
     async def text2img(self, ctx, *prompt):
         # Convert member/channel mentions to text
-        p = []
-        for word in prompt:
-            mention_regex = "^<(@|#)\\d*>$"
-            # e.g. <@111222333444555666>
-            if re.match(mention_regex, word):
-                obj = helpers.get_object_from_mention(ctx, word)
-                if isinstance(obj, discord.Member):
-                    if obj.nick:
-                        word = obj.nick
-                    else:
-                        word = obj.name
-                else:
-                    word = obj.name
-            p.append(word)
-
-        prompt = " ".join(p)
+        prompt = helpers.replace_mentions_with_names(ctx, ' '.join(prompt))
         self.log.info(f"generating image prompt: {prompt}")
         async with ctx.typing():
             image = generate_dezgo_image(prompt, self.bot.config["ai"]["dezgo_api_key"])
             await ctx.send(file=discord.File(image, filename="image.png", spoiler=True))
 
     @commands.command()
-    async def text(self, ctx, *args):
-        pass
+    async def chat(self, ctx, *args):
+        async with ctx.typing():
+            if args:
+                await ctx.send(helpers.generate_openai_chat(
+                    ' '.join(args),
+                    self.bot.config["ai"]["openai_api_key"]
+                ))
+            else:
+                victim = ctx.author.name
+                if ctx.author.nick:
+                    victim = ctx.author.nick
+                await ctx.send(helpers.generate_rude_response_missing_arg(
+                    victim, self.bot.config["ai"]["openai_api_key"]
+                ).strip().strip('"'))
 
 
 async def setup(bot):
@@ -66,7 +70,7 @@ def generate_dezgo_image(
     prompt: str, key: str, model: str = "epic_diffusion_1_1"
 ) -> io.BytesIO:
     url = "https://dezgo.p.rapidapi.com/text2image"
-    payload = {
+    data = {
         "prompt": prompt,
         "guidance": 7,
         "steps": 30,
@@ -81,6 +85,6 @@ def generate_dezgo_image(
         "X-RapidAPI-Key": key,
         "X-RapidAPI-Host": "dezgo.p.rapidapi.com",
     }
-    r = requests.post(url, data=payload, headers=headers)
+    r = requests.post(url, data=data, headers=headers)
 
     return io.BytesIO(r.content)
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,6 +1,11 @@
+from discord.ext import commands
+import discord
 import logging
 from configparser import ConfigParser
+
 import os
+import re
+import requests
 
 _log = logging.getLogger(__name__)
 
@@ -20,10 +25,59 @@ def reload_cfg(path) -> ConfigParser:
     return config
 
 
-def get_object_from_mention(ctx, mention):
+def replace_mentions_with_names(ctx: commands.Context, args: str) -> str:
+    text = []
+    for word in args.split(" "):
+        mention_regex = "^<(@|#)\\d*>$"
+        # e.g. <@111222333444555666>
+        if re.match(mention_regex, word):
+            obj = get_object_from_mention(ctx, word)
+            if isinstance(obj, discord.Member):
+                if obj.nick:
+                    word = obj.nick
+                else:
+                    word = obj.name
+            else:
+                word = obj.name
+        text.append(word)
+
+    return " ".join(text)
+
+
+def get_object_from_mention(ctx: commands.Context, mention: str):
     mentioned_id = int(mention[2:][:-1])
     if mention[1] == "@":
         return ctx.guild.get_member(mentioned_id)
     elif mention[1] == "#":
         return ctx.guild.get_channel_or_thread(mentioned_id)
         pass
+
+
+def generate_openai_chat(
+    prompt: str,
+    key: str,
+    model: str = "gpt-3.5-turbo"
+) -> str:
+    url = "https://api.openai.com/v1/chat/completions"
+    data = {
+        "model": model,
+        "messages": [
+            {"role": "system", "content": "your name is azusa and you're a moody and very rude teenage girl chat bot who never says sorry and calls people bitch a lot."},
+            {"role": "user", "content": prompt}
+        ]
+    }
+    headers = {
+        "content-type": "application/json",
+        "Authorization": f"Bearer {key}"
+    }
+    r = requests.post(url, json=data, headers=headers)
+
+    _log.debug(r.json())
+
+    return r.json()["choices"][0]["message"]["content"]
+
+
+def generate_rude_response_missing_arg(victim: str, key: str) -> str:
+    prompt = f"you were asked to run a command by {victim} but not given all the arguments to do so"
+
+    return generate_openai_chat(prompt, key)
