67735062dcf40fb009c374379b599147a2b922ac
emily <git@emily.ly>
2023-03-12 18:00:12 +0000

    podman

--- Containerfile
+++ Containerfile
@@ -3,3 +3,5 @@ FROM python:3.9-slim
 WORKDIR /persephone
 COPY . .
 RUN pip install -r requirements.txt
+
+CMD python3 run.py
--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 echo "Restarting..."
-docker compose down
-docker compose up --build -d --remove-orphans
+podman stop persephone
+podman build -f Containerfile -t persephone
+podman run -d --name=persephone persephone
 echo "Waiting to verify successful start..."
 sleep 5
 
-if docker compose ls | grep -q ^persephone; then
+if podman ps | grep -q persephone; then
     echo "persephone still running :)"
-    docker compose logs
+    podman logs persephone
     exit 0
 else
     echo "persephone isn't running anymore :("
-    docker compose logs 
+    podman logs persephone
     exit 1
 fi
