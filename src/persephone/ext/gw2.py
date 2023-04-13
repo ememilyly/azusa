3a054452610e1ee59e650870a48cfa2c2226a4a2
emily <git@emily.ly>
2023-04-13 13:23:49 +0000

    Add fotm recs

--- src/persephone/ext/gw2.py
+++ src/persephone/ext/gw2.py
@@ -5,6 +5,28 @@ import requests
 
 _log = logging.getLogger(__name__)
 
+# There's no way to retrieve these through the API so must hardcode :(
+# https://wiki.guildwars2.com/wiki/Fractals_of_the_Mists#Daily_Fractals
+# scrape from wiki?
+# key is the t1 rec as they're always unique and this makes it a lot cleaner
+DAILY_RECS_TABLE = {
+    2: {2: "Uncategorized", 37: "Siren's Reef", 53: "Underground Facility"},
+    6: {6: "Cliffside", 28: "Volcanic", 61: "Aquatic Ruins"},
+    10: {10: "Molten Boss", 32: "Swampland", 65: "Aetherblade"},
+    14: {14: "Aetherblade", 34: "Thaumanova Reactor", 53: "Sunqua Peak"},
+    19: {19: "Volcanic", 37: "Siren's Reef", 66: "Urban Battleground"},
+    15: {15: "Thaumanova Reactor", 41: "Twilight Oasis", 60: "Solid Ocean"},
+    24: {24: "Shattered Observatory", 35: "Solid Ocean", 75: "Sunqua Peak"},
+    25: {25: "Sunqua Peak", 36: "Uncategorized", 69: "Cliffside"},
+    12: {12: "Siren's Reef", 40: "Molten Boss", 67: "Deepstone"},
+    8: {8: "Underground Facility", 31: "Urban Battleground", 54: "Siren's Reef"},
+    11: {11: "Deepstone", 39: "Molten Furnace", 59: "Twilight Oasis"},
+    18: {18: "Captain Mai Trin Boss", 27: "Snowblind", 64: "Thaumanova Reactor"},
+    4: {4: "Urban Battleground", 30: "Chaos", 58: "Molten Furnace"},
+    16: {16: "Twilight Oasis", 42: "Captain Mai Trin Boss", 62: "Uncategorized"},
+    5: {5: "Swampland", 48: "Nightmare", 68: "Snowblind"},
+}
+
 
 class gw2(commands.Cog):
     def __init__(self, bot):
@@ -14,6 +36,7 @@ class gw2(commands.Cog):
     @commands.command(aliases=("fotm",), help="Check daily fractals")
     async def fractals(self, ctx, tomorrow: str = None):
         endpoint = "daily/tomorrow" if tomorrow == "tomorrow" else "daily"
+        message = ["Daily tiered:"]
         async with ctx.typing():
             # get all dailies
             dailies = request_gw2_api(f"achievements/{endpoint}")
@@ -22,11 +45,22 @@ class gw2(commands.Cog):
             fractal_achieves = request_gw2_api(
                 "achievements", params={"ids": fractal_ids}
             )
-            fractal_names = [
+            message += [
+                # "Daily Tier X Fractal" -> "Fractal"
                 i["name"][13:] for i in fractal_achieves if "Tier 4" in i["name"]
             ]
+            # look up recs
+            message.append("\nDaily recs:")
+            rec_scales = [
+                # "Daily Recommended Fractal-Scale XX" -> XX
+                int(i["name"].split()[-1]) for i in fractal_achieves if "Recommended" in i["name"]
+            ]
+            t1_rec = sorted(rec_scales)[0]
+            daily_recs = DAILY_RECS_TABLE[t1_rec]
+            for scale, name in daily_recs.items():
+                message.append(f"{name} (rec {scale})")
 
-            await ctx.reply("\n".join(fractal_names))
+            await ctx.reply("\n".join(message))
 
 
 async def setup(bot):
@@ -38,5 +72,5 @@ def request_gw2_api(endpoint: str, params: dict = {}) -> list:
     url = f"https://api.guildwars2.com/v2/{endpoint}"
 
     r = requests.get(url, params=params, timeout=20)
-    _log.warning(r.json())
+    _log.debug(r.json())
     return r.json()
